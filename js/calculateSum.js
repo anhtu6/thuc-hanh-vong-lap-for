d5e61a2fc6c5e8448f05cb5ee75612d4c9370195
anhtu6 <anhtu632@gmail.com>
2020-02-23 18:25:52 +0700

    thuc hanh vong lap for

---
+++ js/calculateSum.js
@@ -0,0 +1,6 @@
+let n = parseInt(prompt('Nhap so cua ban'));
+let sum = 0;
+for (i=1; i<=n; i++) {
+    sum += i;
+}
+alert('tong cac so la '+sum);
---
+++ js/showNumber.js
@@ -0,0 +1,6 @@
+let i = 0;
+let text = '';
+for (i = 0; i <5; i++) {
+    text += "The number is " + i +"<br>";
+}
+document.getElementById('demo').innerHTML =  text;
